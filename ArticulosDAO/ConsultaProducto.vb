870c5bb264a57f6bec0c0a2e4ee18ee1b13fbc5f
SQA <perezjefersonjdpc@hotmail.com>
2020-08-26 12:54:36 -0500

    repositorio prueba ingreso

---
+++ ArticulosDAO/ConsultaProducto.vb
@@ -0,0 +1,3 @@
+﻿Public Class ConsultaProducto
+
+End Class
---
+++ ProductosDAO/Class1.vb
@@ -0,0 +1,11 @@
+Imports System.Data
+
+Public Class Class1
+
+	Public Function JsonToDt(ByVal jsonDatos As String)
+		Dim dt As New DataTable
+
+		Return ""
+	End Function
+
+End Class
